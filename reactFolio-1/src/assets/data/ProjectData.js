e10aac855688f83eb659a4a84ec1ecc9418d0e95
hye-ji <55532618+hye-ji@users.noreply.github.com>
2025-04-22 17:07:36 +0900

    Add files via upload
    basic

---
+++ reactFolio-1/src/assets/data/ProjectData.js
@@ -0,0 +1,30 @@
+const projectData = {
+    projectList: [
+        {
+            name: "지역상품권 지급결제플랫폼 운영 및 유지관리",
+            period: "2023.11 - 2024.10",
+            description: "한국조폐공사 프로젝트입니다. - 지역상품권 사이트 개발 및 유지관리를 하였습니다. 프로젝트 진행중에 투입되어 약 6개월간 진행하였습니다.",
+            stack: ["Java", "React", "Oracle", "Database"]
+        },
+        {    
+            name: "한-인도 전자적 원산지정보 교환 시스템(EODES) 구축",
+            period: "2023.02 - 2023.10",
+            description: "관세청 프로젝트입니다. 한-인도 연결을 제외하고 원산지정보 교환 페이지 전부 생성하였습니다.",
+            stack: ["Java", "Javascript", "Html", "Database"]
+        },
+        {    
+            name: "납세신고 도움정보 시스템 고도화 사업",
+            period: "2022.07 - 2023.01",
+            description: "관세청의 납세신고 프로젝트입니다. API를 이용하여 Ajax로 호출하여 차트 시각화하였습니다.",
+            stack: ["Java", "Javascript", "Html","API", "Ajax", "Database"]
+        },
+        {
+            name: "AI기반 스마트 종합정보 포털시스템 구축사업",
+            period: "2021.02 - 2022.01",
+            description: "한국해양교통안전공단 프로젝트입니다. 페이지 테스트간 오류수정 및 개발작업을 하였습니다. 프로젝트 진행중에 투입되어 약 11개월간 진행하였습니다.",
+            stack: ["Java", "Javascript", "Html", "Database"]
+        }
+    ]
+}
+
+export default projectData;
